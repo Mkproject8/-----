5633b073952b50876c09a06b168dbfaa4c3cdf91
cracker <96644787+Mkproject8@users.noreply.github.com>
2022-12-29 23:13:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -239,7 +239,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/OpusTechz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('ᎠᎬᏙᏞϴᏢᎬᎡ🧑‍💻', url='https://t.me/doctor6689') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
